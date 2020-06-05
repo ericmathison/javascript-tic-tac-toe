3b56e47904120da377c84ab289739169f7505abb
Eric Mathison <dev@ericmathison.io>
2020-06-04 23:59:15 -0700

    Implement move() method

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,4 +2,14 @@ class Board {
   constructor() {
     this.state = '---------';
   }
+
+  move(index, player) {
+    if (index == 0) {
+      this.state = player + this.state.slice(index + 1, this.state.length + 1);
+    } else if (index == 8) {
+      this.state = this.state.slice(0, index) + player;
+    } else {
+      this.state = this.state.slice(0, index) + player + this.state.slice(index + 1, this.state.length + 1);
+    }
+  }
 }
